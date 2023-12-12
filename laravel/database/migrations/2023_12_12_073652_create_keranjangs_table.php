6abba8f247463deea5b642675174718dc35f17fc
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2023-12-12 23:10:01 +0700

    zzz

--- laravel/database/migrations/2023_12_12_073652_create_keranjangs_table.php
+++ laravel/database/migrations/2023_12_12_073652_create_keranjangs_table.php
@@ -11,17 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('keranjang', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('alamat');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->decimal('total_harga', 10, 2);
             $table->timestamps();
-            
-
         });
     }
 
@@ -30,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('keranjangs');
     }
 };
